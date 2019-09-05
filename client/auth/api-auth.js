292baff948ffdb438e61410e799b82a0df3e9511
rhill34 <rhill34@mail.greenriver.edu>
2019-09-05 15:23:53 -0700

    WIP:DONE FETCH for auth API. @RH3
    Methods for Sign-In, and Sign-Out created.

---
+++ client/auth/api-auth.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * FETCH for AUTH API
+ * 
+ */
+const signin = (user) => {
+    return fetch('/auth/signin/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(user)
+    })
+    .then((response) => {
+        return response.json()
+    }).catch((err) => console.log(err))
+}
+
+const signout =() => {
+    return fetch('/auth/signout/', {
+        method: 'GET',
+    }).then(response => {
+        return response.json()
+    }).catch((err) => console.log(err))
+}
+
+export {
+    signin,
+    signout
+}
