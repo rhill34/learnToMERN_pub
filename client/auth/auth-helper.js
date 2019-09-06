d054dc83e238a3c2beb92d0f7d0df08cfb79252b
rhill34 <rhill34@mail.greenriver.edu>
2019-09-05 23:02:28 -0700

    WIP:DID PrivateRoute & Cleanup of Managing auth state. @RH3
    Created const auth to export the functions from auth-helpers.js. Created redirect for the PrivateRoute.js

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -1,33 +1,37 @@
-import { TableBody } from "material-ui"
+import {signout} from './api-auth'
 
-/**
- * Save Credentials
- */
-AuthenticatorResponse(jwt, cb) {
-    if(typeof window !== "undefined")
-        sessionStorage.setItem('jwt', JSON.stringify(jwt))
-    cb()
-}
-/**
- * Retrieve credentials if signed-in:
- */
-isAuthenticated() {
-    if (typeof window == "undefined")
-        return false
+const auth = {
+    /**
+     * Save Credentials
+     */
+    Authenticate(jwt, cb) {
+        if(typeof window !== "undefined")
+            sessionStorage.setItem('jwt', JSON.stringify(jwt))
+        cb()
+    },
+    /**
+     * Retrieve credentials if signed-in:
+     */
+    isAuthenticated() {
+        if (typeof window == "undefined")
+            return false
 
-    if(sessionStorage.getItem('jwt'))
-        return JSON.parse(sessionStorage.getItem('jwt'))
-    else 
-        return false
+        if(sessionStorage.getItem('jwt'))
+            return JSON.parse(sessionStorage.getItem('jwt'))
+        else 
+            return false
+    },
+    /**
+     * Delete credentials and sign out
+     */
+    signout(cb) {
+        if(typeof window !== "undefined")
+            sessionStorage.removeItem('jwt')
+        cb()
+        signout().then((data) => {
+            document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+        })
+    }
 }
-/**
- * Delete credentials and sign out
- */
-signout(cb) {
-    if(typeof window !== "undefined")
-        sessionStorage.removeItem('jwt')
-    cb()
-    signout().then((data) => {
-        document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-    })
-}
+
+export default auth
